0591e9650d79606f30082d2636e1dd137516b724
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2023-12-22 17:39:48 +0530

    String Formatting

---
+++ String_formating.py
@@ -0,0 +1,10 @@
+"""
+string Formatting Operator
+1. String Formatting Operator(%)is used to format a string
+2. %d , %c , %s and %f are some commonly used string formatters
+"""
+#For Example
+age=28
+print("My Age is %d"%(age))
+
+#Since 28 is an integer, therefore I have specified %d and the number 28 will be placed at %d.
