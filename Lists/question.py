b97e30dd1a5f06dd55e1249ee25c31ccf0e866ed
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2023-12-26 17:09:17 +0530

    lists

---
+++ Lists/question.py
@@ -0,0 +1,15 @@
+animals=['lion','tiger','monkey','elephant','frog']
+
+filtered_animal=[]
+for animal in animals:
+     filtered_animal.append(animal.title())
+
+print(filtered_animal)
+
+
+animals = ['lion', 'tiger', 'monkey', 'elephant', 'frog']
+
+filtered_animals = [animal.title() for animal in animals]
+
+print(filtered_animals)
+
