29e6286e7ffd045eb24f7f61e31a6fca50871d73
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2023-12-26 15:54:51 +0530

    Input

---
+++ input.py
@@ -0,0 +1,28 @@
+# INPUT() METHOD
+"""Used to take input from the user"""
+"""User input is always converted to a string"""
+
+#for example
+name=input()
+print(name)
+print(type(name))
+
+
+#INPUT METHOD WITH A MESSAGE
+
+"""Syntax: Input('Message')"""
+number=input('Enter a number: ')
+print(number)
+print(type(number))
+
+
+#Typecasting the user input
+"""Typecasting is needed to convert the string to integer"""
+"""Input() Method can be provided as an arguement to the int() method"""
+ 
+#FOR EXAMPLE
+number=int(input("Enter the number: "))
+print(number)
+print(type(number))
+
+
